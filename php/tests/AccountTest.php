255c49219d2b81a7e639d491105cc81e920f1b9a
xitong <xitong@thenetcircle.com>
2025-04-01 13:43:17 +0800

    TDD step 3: add multiple transactions (Red)

--- php/tests/AccountTest.php
+++ php/tests/AccountTest.php
@@ -27,4 +27,33 @@ EOT;
 
         $this->assertEquals($expected, $output);
     }
+
+    public function test_multiple_transactions_print_in_reverse_order_with_running_balance()
+    {
+        $account = new Account();
+
+        $account->setDate('10/01/2012');
+        $account->deposit(1000);
+
+        $account->setDate('13/01/2012');
+        $account->deposit(2000);
+
+        $account->setDate('14/01/2012');
+        $account->withdraw(500);
+
+        ob_start();
+        $account->printStatement();
+        $output = ob_get_clean();
+
+        $expected = <<<EOT
+Date       || Amount || Balance
+14/01/2012 ||  -500  || 2500
+13/01/2012 ||  2000  || 3000
+10/01/2012 ||  1000  || 1000
+
+EOT;
+
+        $this->assertEquals($expected, $output);
+    }
+
 }

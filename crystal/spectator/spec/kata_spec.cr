df6196f05e4f3fc63cc524742a4a74383b5f3610
xitong <xitong@thenetcircle.com>
2025-04-01 11:32:23 +0800

    init php

--- crystal/spectator/spec/kata_spec.cr
+++
@@ -1,7 +0,0 @@
-require "./spec_helper"
-
-Spectator.describe Kata do
-  it "greets the given name" do
-    expect(described_class.greet("World")).to eq "fail"
-  end
-end
--- crystal/spectator/src/kata.cr
+++
@@ -1,5 +0,0 @@
-module Kata
-  def self.greet(name)
-    "Hello #{name}!"
-  end
-end
